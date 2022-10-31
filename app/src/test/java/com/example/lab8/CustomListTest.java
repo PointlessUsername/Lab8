caa050e474e943a26fad2408f07da673b5062daf
Yahya <42281247+PointlessUsername@users.noreply.github.com>
2022-10-30 22:20:40 -0600

    Test 2

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-    
+
     private CustomList list;
     /**
      * create a mocklist for my citylist
